bf9301dd149f4b5aa544405496896a0e2095527c
Carrotlord <j.oliverchu@berkeley.edu>
2014-11-22 01:13:49 -0800

    Pawns will highlight squares they can move to.

--- chess_commentator.js
+++ chess_commentator.js
@@ -1,7 +1,152 @@
+var ILLEGAL_LOCATION = "illegal_location";
+
 var WHITE = 0;
 var BLACK = 1;
+var NEITHER = 2;
+
+var KING = 0;
+var QUEEN = 1;
+var ROOK = 2;
+var BISHOP = 3;
+var KNIGHT = 4;
+var PAWN = 5;
+var EMPTY = 6;
+
+var NONE = 0;
+var MOVE_TO = 1;
+
+function Piece(color, type) {
+    this.type = type;
+    this.color = color;
+}
+
+Piece.prototype.isEmpty = function isEmpty() {
+    return this.type === EMPTY && this.color === NEITHER;
+}
+
+Piece.prototype.equals = function equals(otherPiece) {
+    return this.type === otherPiece.type && this.color === otherPiece.color;
+}
+
+/* TODO: Implement this */
+Piece.prototype.toString = function toString() {
+    return "";
+}
+
+/* TODO: Finish implementing this */
+Piece.prototype.legalMoves = function legalMoves(location) {
+    var isOnHomeRow = function isOnHomeRow(location, color) {
+        var coords = getCoords(location);
+        var j = coords[1];
+        if (color === BLACK) {
+            return j === 1;
+        } else {
+            return j === 6;
+        }
+    }
+    /** Given an array of coordinates [[i,j], [i,j]...]
+     *  returns all the locations ["a1", "b2"...], getting
+     *  rid of all illegal locations.
+     */
+    var toLegalLocations = function toLegalLocations(coordsArray) {
+        var locations = coordsArray.map(coordsToLocation)
+        var legalLocations = locations.filter(isLegalLocation);
+        return legalLocations;
+    }
+    if (this.isEmpty()) {
+        return [];
+    }
+    var coords = getCoords(location);
+    var i = coords[0];
+    var j = coords[1];
+    if (this.type === PAWN) {
+        if (this.color === BLACK) {
+            /* Black moves downward. */
+            if (isOnHomeRow(location, BLACK)) {
+                /* Pawns can move 1 or 2 squares down when on the home row. */
+                return toLegalLocations([[i, j + 1], [i, j + 2]]);
+            } else {
+                return toLegalLocations([[i, j + 1]]);
+            }
+        } else {
+            /* White moves upward. */
+            if (isOnHomeRow(location, WHITE)) {
+                return toLegalLocations([[i, j - 1], [i, j - 2]]);
+            } else {
+                return toLegalLocations([[i, j - 1]]);
+            }
+        }
+    } /* TODO: else if... */
+    return [];
+}
+
+function Board() {
+    var blackKing = new Piece(BLACK, KING);
+    var blackQueen = new Piece(BLACK, QUEEN);
+    var blackRook = new Piece(BLACK, ROOK);
+    var blackBishop = new Piece(BLACK, BISHOP);
+    var blackKnight = new Piece(BLACK, KNIGHT);
+    var blackPawn = new Piece(BLACK, PAWN);
+    var whiteKing = new Piece(BLACK, KING);
+    var whiteQueen = new Piece(WHITE, QUEEN);
+    var whiteRook = new Piece(WHITE, ROOK);
+    var whiteBishop = new Piece(WHITE, BISHOP);
+    var whiteKnight = new Piece(WHITE, KNIGHT);
+    var whitePawn = new Piece(WHITE, PAWN);
+    var emptyPiece = new Piece(NEITHER, EMPTY);
+    this.contents = [[blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop, blackKnight, blackRook],
+                     [blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn],
+                     [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                     [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                     [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                     [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                     [whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn],
+                     [whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop, whiteKnight, whiteRook]];
+}
+
+Board.prototype.get = function get(squareString) {
+    var coords = getCoords(squareString);
+    var i = coords[0];
+    var j = coords[1];
+    return this.contents[j][i];
+}
+
+Board.prototype.isEmptyAt = function isEmptyAt(location) {
+    return this.get(location).isEmpty();
+}
+
+/** Returns true if the square is empty or can be attacked. */
+Board.prototype.isConsumable = function isConsumable(location, attackerColor) {
+    var targetedPiece = this.get(location);
+    return this.isEmptyAt(location) || targetedPiece.color !== attackerColor;
+}
+
+function finalizeLegalMoves(currentMoves, attackerColor) {
+    var finalizedMoves = [];
+    for (var i = 0; i < currentMoves.length; ++i) {
+        var location = currentMoves[i];
+        if (g_board.isConsumable(location, attackerColor)) {
+            finalizedMoves.push(location);
+        }
+    }
+    return finalizedMoves;
+}
+
+function makeEmptyMoveToBoard() {
+    var board = [];
+    for (var i = 0; i < 8; ++i) {
+        var row = [];
+        for (var j = 0; j < 8; ++j) {
+            row.push(NONE);
+        }
+        board.push(row);
+    }
+    return board;
+}
 
 var g_currentlySelectedSquareString = null;
+var g_board = new Board();
+var g_moveToBoard = makeEmptyMoveToBoard();
 
 function getCoords(squareString) {
     var aCharCode = "a".charCodeAt(0);
@@ -12,6 +157,19 @@ function getCoords(squareString) {
     }
     return [squareString.charCodeAt(0) - aCharCode,
             squareString.charCodeAt(1) - oneCharCode];
+}
+
+function isLegalLocation(location) {
+    return ILLEGAL_LOCATION !== location;
+}
+
+function coordsToLocation(coords) {
+    var i = coords[0];
+    var j = coords[1];
+    if (i < 0 || i > 7 || j < 0 || j > 7) {
+        return ILLEGAL_LOCATION;
+    }
+    return "abcdefgh"[i] + "12345678"[j];
 }
 
 function getBaseColor(squareString) {
@@ -46,12 +204,44 @@ function getTileColor(color) {
     }
 }
 
+function displayPossibleMoves(moves) {
+    /* Update the moves-to table and the DOM. */
+    for (var k = 0; k < moves.length; ++k) {
+        var coords = getCoords(moves[k]);
+        var i = coords[0];
+        var j = coords[1];
+        g_moveToBoard[j][i] = MOVE_TO;
+        activateMoveTo(moves[k]);
+    }
+}
+
+function activateMoveTo(squareString) {
+    var square = document.getElementById(squareString);
+    square.className = "move_to_tile";
+}
+
+/** Removes all move-to tiles. */
+function clearAllMoveTo() {
+    for (var i = 0; i < 8; ++i) {
+        for (var j = 0; j < 8; ++j) {
+            var location = coordsToLocation([i, j]);
+            var square = document.getElementById(location);
+            if (square.className === "move_to_tile") {
+                square.className = getTileColor(getBaseColor(location));
+            }
+        }
+    }
+}
+
 function toggleSquare(squareString) {
+    clearAllMoveTo();
     var square = document.getElementById(squareString);
     if (square.className === "selected_tile") {
         /* Turn off the square that's selected: */
         square.className = getTileColor(getBaseColor(squareString));
         g_currentlySelectedSquareString = null;
+        /* Destroy the move-to board. */
+        g_moveToBoard = makeEmptyMoveToBoard();
     } else {
         /* Turn off any selected squares, and then select our new square.
          * In this case, the new square cannot be the one already selected. */
@@ -60,5 +250,11 @@ function toggleSquare(squareString) {
         }
         square.className = "selected_tile";
         g_currentlySelectedSquareString = squareString;
+        /* Now it's time to get our possible moves. */
+        var attacker = g_board.get(squareString);
+        var possibleMoves = attacker.legalMoves(squareString);
+        var actualMoves = finalizeLegalMoves(possibleMoves, attacker.color);
+        /* Then display possible moves. */
+        displayPossibleMoves(actualMoves);
     }
 }
