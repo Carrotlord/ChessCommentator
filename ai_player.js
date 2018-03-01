da72cc3499566c6ac59d8659bc82fb14693619f1
Carrotlord <j.oliverchu@gmail.com>
2018-02-28 20:53:49 -0800

    Attempt at negamax.

--- ai_player.js
+++ ai_player.js
@@ -5,6 +5,28 @@ function startGameAgainstAI(level) {
         aiSayComment("You are now playing against the beginner AI.");
     } else if (level === "intermediate") {
         g_against = AI_INTERMEDIATE;
+        var blackKing = new Piece(BLACK, KING);
+        var blackQueen = new Piece(BLACK, QUEEN);
+        var blackRook = new Piece(BLACK, ROOK);
+        var blackBishop = new Piece(BLACK, BISHOP);
+        var blackKnight = new Piece(BLACK, KNIGHT);
+        var blackPawn = new Piece(BLACK, PAWN);
+        var whiteKing = new Piece(WHITE, KING);
+        var whiteQueen = new Piece(WHITE, QUEEN);
+        var whiteRook = new Piece(WHITE, ROOK);
+        var whiteBishop = new Piece(WHITE, BISHOP);
+        var whiteKnight = new Piece(WHITE, KNIGHT);
+        var whitePawn = new Piece(WHITE, PAWN);
+        var emptyPiece = new Piece(NEITHER, EMPTY);
+        g_board.contents = [[emptyPiece, emptyPiece, blackQueen, emptyPiece, blackKing, emptyPiece, emptyPiece, emptyPiece],
+                            [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                            [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                            [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                            [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                            [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                            [emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece, emptyPiece],
+                            [emptyPiece, emptyPiece, emptyPiece, whiteQueen, whiteKing, emptyPiece, emptyPiece, emptyPiece]];
+        reloadGraphical();
         aiSayComment("You are now playing against the intermediate AI.");
     }
     g_aiPlayer = new AIPlayer(level);
@@ -21,7 +43,7 @@ function isMoveAcceptable(move, board) {
     return !virtualBoard.isKingInCheck(BLACK);
 }
 
-function getAILegalMoves(board) {
+function getAILegalMoves(board, color) {
     var allMoves = [];
     var currentSquare = null;
     var columns = "abcdefgh";
@@ -31,7 +53,7 @@ function getAILegalMoves(board) {
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
             currentSquare = columns[i] + rows[j];
-            moves = getAllLegalMoves(currentSquare, board, BLACK);
+            moves = getAllLegalMoves(currentSquare, board, color);
             for (var k = 0; k < moves.length; k++) {
                 currentMove = {from: currentSquare, to: moves[k]};
                 if (isMoveAcceptable(currentMove, board)) {
@@ -44,15 +66,13 @@ function getAILegalMoves(board) {
 }
 
 AIPlayer.prototype.nextMove = function(board) {
-    var allMoves = getAILegalMoves(board);
+    var allMoves = getAILegalMoves(board, BLACK);
     var selectedMove = {from: "a1", to: "a1"};
     if (allMoves.length === 0) {
         aiSayComment("Player black gives up! No legal moves!");
         return selectedMove;
     }
     if (this.level === "beginner") {
-        selectedMove = allMoves[randInt(0, allMoves.length - 1)];
-    } else if (this.level === "intermediate") {
         var virtualBoard = null;
         var bestMove = null;
         var bestPoints = null;
@@ -75,6 +95,11 @@ AIPlayer.prototype.nextMove = function(board) {
             }
         }
         selectedMove = equalMoves[randInt(0, equalMoves.length - 1)];
+    } else if (this.level === "intermediate") {
+        var result = this.negamax(board, 4, BLACK, []);
+        console.log(result.chain);
+        console.log(result.value);
+        selectedMove = result.chain[0];
     }
     commentate(selectedMove.from, selectedMove.to, BLACK,
                board.get(selectedMove.from), board.get(selectedMove.to));
@@ -98,6 +123,40 @@ AIPlayer.prototype.evaluateBoard = function(board) {
         }
     }
     return points;
+}
+
+/* Warning: depth should be an even number */
+AIPlayer.prototype.negamax = function(board, depth, color, moveChain) {
+    var moves = getAILegalMoves(board, color);
+    if (depth <= 0 || moves.length === 0) {
+        /* Evaluation is done, so return value of leaf node */
+        if (color === WHITE) {
+            return {chain: moveChain, value: this.evaluateBoard(board)};
+        } else {
+            return {chain: moveChain, value: -this.evaluateBoard(board)};
+        }
+    } else {
+        var bestValue = null;
+        var bestMove = null;
+        var currentValue = null;
+        var currentMove = null;
+        var nextBoard = null;
+        var result = null;
+        for (var i = 0; i < moves.length; i++) {
+            currentMove = moves[i];
+            nextBoard = copyBoard(board);
+            makeVirtualMove(currentMove.from, currentMove.to, nextBoard);
+            currentMove.color = getPlayer(color);
+            result = this.negamax(nextBoard, depth - 1, getOppositeColor(color), moveChain.concat([currentMove]));
+            currentValue = -result.value;
+            if (bestValue === null || currentValue > bestValue) {
+                bestValue = currentValue;
+                bestMove = currentMove;
+                moveChain = result.chain;
+            }
+        }
+        return {chain: moveChain, value: bestValue};
+    }
 }
 
 function requestAIMove(aiPlayer, board) {
